ab4a9b744137389e312bbd369294599bccbdae9a
Manav Arora <manav.a@keka.com>
2023-03-06 00:50:20 +0530

    First Commit For New Portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MoreComponent } from './more/more.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home' , pathMatch: 'full'},
+  { path: 'home',component: HomePageComponent },
+  { path: 'more', component: MoreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

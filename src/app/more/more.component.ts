775901844e49d2d6acad2d78211e338eaf04f510
Manav Arora <manav.a@keka.com>
2023-04-28 00:02:41 +0530

    Added Footer

--- src/app/more/more.component.ts
+++ src/app/more/more.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-more',
   templateUrl: './more.component.html',
   styleUrls: ['./more.component.scss']
 })
-export class MoreComponent {
+export class MoreComponent implements OnInit {
 
+  scrollPercent:any;
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (): void => {
+    this.scrollPercent = Math.round((document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 100);
+  };
+  
 }
